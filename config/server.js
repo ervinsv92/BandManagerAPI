c1236d6fe32467a3c79c64f66766a2d95cd34ad2
ervinsv92 <ervinsv92@gmail.com>
2021-09-22 21:51:47 -0600

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f23c3a6724ac420977a5428228abd0b2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '00665bc8-ace5-4737-a4bb-aa76690adfb4'
+};
